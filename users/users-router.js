70a9c88e5d63aa59e50093ff3157952ae31d46c2
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-26 23:13:13 -0500

    working on hashing register

--- users/users-router.js
+++ users/users-router.js
@@ -18,7 +18,38 @@ router.get('/register', (req, res) => {
 
 })
 
-// router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+    let user = req.body;
+
+    //if missing username or password during registration
+    if(!user.username || !user.password) {
+        res.status(404).json({message: 'Missing Username or Password. Please reenter credentials'})
+    } 
+
+    //if has username and password then...
+    if(user.username && user.password) {
+        const hash = bcrypt.hashSync(user.password, 12)
+
+        user.password = hash;
+
+        Users.add(uesr)
+            .then(saved => {
+                const token = genToken(saved)
+                //same as token: token
+                res.status(201).json({created_user: saved, token})
+            })
+            .catch(err => {
+                res.status(500).json({message: "Error Creating User in the Database", err})
+            })
+    }   else {
+        res.status(400).json
+    }
+
+    
+
+
+
+});
 
 router.get("/login", (req, res) => {
     Users.find()
